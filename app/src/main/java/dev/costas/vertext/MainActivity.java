8296842d4e5e8f30e8368dec17bb107d03e9f940
Ariel Costas <arielcostas@mailbox.org>
2022-07-27 00:02:57 +0200

    Change button to menu, show file name on top

--- app/src/main/java/dev/costas/vertext/MainActivity.java
+++ app/src/main/java/dev/costas/vertext/MainActivity.java
@@ -2,10 +2,14 @@ package dev.costas.vertext;
 
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.text.Html;
 import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 import androidx.activity.result.ActivityResult;
@@ -19,36 +23,56 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mainmenu_abrir) {
+            openFile();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title + " - VerText");
+    }
+
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
+            result -> {
                 if(result.getResultCode() == RESULT_OK) {
                     Intent data = result.getData();
+                    assert data != null;
                     Uri fileUri = data.getData();
                     InputStream is;
-                    var sb = new StringBuffer();
+                    var sb = new StringBuilder();
                     try {
                         is = getContentResolver().openInputStream(fileUri);
                         Scanner scanner  =  new Scanner(is);
                         while (scanner.hasNextLine()) {
-                            sb.append(scanner.nextLine() + "\n");
+                            sb.append(scanner.nextLine());
+                            sb.append("\n");
                         }
                     } catch (IOException e) {
-                        setWebViewContent(e.getStackTrace().toString());
+                        setWebViewContent(Arrays.toString(e.getStackTrace()));
                         return;
                     }
 
+                    setTitle(getFilenameFromUri(fileUri));
                     setWebViewContent(sb.toString());
                 }
             }
-        }
     );
 
     @Override
@@ -56,42 +80,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.vistaweb);
-        
-        ((FloatingActionButton) findViewById(R.id.fab)).setOnClickListener(v -> {
-            openFile();
-        });
 
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
 
+        Uri uri;
+
         switch(action) {
             case Intent.ACTION_EDIT:
             case Intent.ACTION_VIEW:
                 String entrada = intent.getStringExtra(Intent.EXTRA_TEXT);
+                uri = Uri.parse(intent.getStringExtra(Intent.EXTRA_ORIGINATING_URI));
+                setTitle(getFilenameFromUri(uri));
                 setWebViewContent(Html.escapeHtml(entrada));
                 break;
             case Intent.ACTION_SEND:
-                Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
                 String text = intent.getStringExtra(Intent.EXTRA_TEXT);
 
                 if (uri != null) {
+                    setTitle(getFilenameFromUri(uri));
                     try {
                         AssetFileDescriptor afd =  getContentResolver().openTypedAssetFileDescriptor(uri, type, null);
                         StringBuilder sb =  new StringBuilder();
                         Scanner scanner = new Scanner(afd.createInputStream());
                         while(scanner.hasNextLine()) {
                             sb.append(scanner.nextLine() + "\n");
+                            sb.append("\n");
                         }
                         setWebViewContent(sb.toString());
-                    } catch (FileNotFoundException e) {
-                        setWebViewContent(e.getStackTrace().toString());
-                        return;
                     } catch (IOException e) {
-                        setWebViewContent(e.getStackTrace().toString());
+                        setWebViewContent(Arrays.toString(e.getStackTrace()));
                         return;
                     }
                 } else {
+                    setTitle("Compartido");
                     setWebViewContent(text);
                 }
                 break;
@@ -116,5 +140,18 @@ public class MainActivity extends AppCompatActivity {
         String contenido = "<html><body><i>Ningún archivo abierto</i></body></html>";
         String encoded = Base64.encodeToString(contenido.getBytes(), Base64.NO_PADDING);
         webView.loadData(encoded, "text/html", "base64");
+    }
+
+    public String getFilenameFromUri(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        if (cursor == null) {
+            return "Desconocido"; // TODO: Translations
+        }
+
+        int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        cursor.moveToFirst();
+        String name = cursor.getString(nameIndex);
+        cursor.close();
+        return name;
     }
 }
