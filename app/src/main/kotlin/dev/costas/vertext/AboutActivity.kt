cb1438156043a5a5d05a5e70f322cb56c0481a11
Ariel Costas <arielcostas@mailbox.org>
2022-08-29 14:51:24 +0200

    Implement status bar color

--- app/src/main/kotlin/dev/costas/vertext/AboutActivity.kt
+++ app/src/main/kotlin/dev/costas/vertext/AboutActivity.kt
@@ -3,6 +3,7 @@ package dev.costas.vertext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,13 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.preference.PreferenceManager
 import dev.costas.vertext.theme.VerTextTheme
 
 class AboutActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
 		setContent {
-			VerTextTheme {
+			VerTextTheme(
+				darkTheme = when (
+					prefs.getString("theme", "MODE_NIGHT_FOLLOW_SYSTEM")) {
+					"MODE_NIGHT_NO" -> false
+					"MODE_NIGHT_YES" -> true
+					else -> isSystemInDarkTheme()
+				}
+			) {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
