e2a03b8c54f4694d9534b54603969b5b98124b7d
Ariel Costas <arielcostas@mailbox.org>
2022-09-22 21:42:08 +0200

    Add about items

--- app/src/main/kotlin/dev/costas/vertext/data/AboutItems.kt
+++ app/src/main/kotlin/dev/costas/vertext/data/AboutItems.kt
@@ -1,9 +1,10 @@
 package dev.costas.vertext.data
 
+import dev.costas.vertext.BuildConfig
 import dev.costas.vertext.R
 import dev.costas.vertext.models.AboutItem
 
-val AboutItems = listOf<AboutItem>(
+val AboutItems = listOf(
 	AboutItem(
 		R.string.about_sourcecode,
 		"GitHub",
@@ -15,5 +16,17 @@ val AboutItems = listOf<AboutItem>(
 		"Crowdin",
 		R.drawable.ic_translate_24,
 		"https://crowdin.com/project/vertext"
+	),
+	AboutItem(
+		R.string.about_version,
+		"Tag ${BuildConfig.VERSION_NAME}",
+		R.drawable.ic_version_24,
+		"https://github.com/arielcostas/vertext/releases/tag/${BuildConfig.VERSION_NAME}"
+	),
+	AboutItem(
+		R.string.about_license_title,
+		"Apache Public License 2.0",
+		R.drawable.ic_license_24,
+		"https://github.com/arielcostas/vertext/blob/main/LICENSE"
 	)
 )
