1ed8a2a2caa21f119dea2488474ceb4af84cf1db
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-01-11 13:08:43 +0100

    Use dynamic colors when available

--- app/src/main/kotlin/dev/costas/vertext/ui/theme/Theme.kt
+++ app/src/main/kotlin/dev/costas/vertext/ui/theme/Theme.kt
@@ -1,11 +1,11 @@
 package dev.costas.vertext.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -36,7 +36,10 @@ fun VerTextTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	content: @Composable () -> Unit
 ) {
+	val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
 	val colorScheme = when {
+		dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+		dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
 		darkTheme -> DarkColorScheme
 		else -> LightColorScheme
 	}
